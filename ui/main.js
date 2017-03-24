6d2cb08102b8325a0e91c1a08dddc4bbe58aea9b
geidhal <geidhaljoseph@gmail.com>
2017-03-24 20:42:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,10 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 var img = document.getElementById('madi');
+function moveRight() {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick=function () {
     var interval = setInterval(moveRight, 100);
     img.style.marginLeft='100px';
