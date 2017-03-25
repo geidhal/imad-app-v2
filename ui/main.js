3b6af483cc98c1a4683825d80a10b055846c6ed1
geidhal <geidhaljoseph@gmail.com>
2017-03-25 12:05:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,8 +11,7 @@ button.onClick = function () {
             if(request.status == 200){
                 var counter = request.responseText;
                 var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
+                span.innerHTML = counter;
         }
     }
     
