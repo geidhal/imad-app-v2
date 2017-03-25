0dfcf01c8b9a30c9f1bae2eeee4a232e0c597e7b
geidhal <geidhaljoseph@gmail.com>
2017-03-25 10:32:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,9 @@
-console.log('Loaded!');
-var element = document.getElementById('main-text');
-element.innerHTML = 'Aswin.., Click on the above Pic';
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight() {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick=function () {
-    var interval = setInterval(moveRight, 20);
+var button=document.getelementById('counter');
+
+var counter = 0;
+
+button.onClick = function() {
+    counter=counter + 1;
+    var span = document.getelementbyId('count');
+    span.innerHTML=counter.toString();
 };
